7427a8af496b0780347cbd5ce413c4d256eabaee
Bloomand <geldashal@gmail.com>
2025-02-02 19:33:45 +0300

    feat(ui): implement responsive design for Precise-menu page

--- components/Precise/Precise.js
+++ components/Precise/Precise.js
@@ -7,19 +7,16 @@ import {
   TouchableOpacity,
   Switch,
   Image,
+  Dimensions
 } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 const DATA_difficult = [
   { id: '0', title: 'Easy' },
   { id: '1', title: 'Medium' },
   { id: '2', title: 'Hard' }
 ];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, { backgroundColor }]}>
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-  </TouchableOpacity>
-);
 
 const DATA_type = [
   { id: '0', title: '+' },
@@ -29,180 +26,131 @@ const DATA_type = [
   { id: '4', title: '%' },
   { id: '5', title: 'Random' }
 ];
-const Item2 = ({ item, onPress, backgroundColor, textColor }) => (
+
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, { backgroundColor }]}>
     <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
   </TouchableOpacity>
 );
 
-
-
 const Precise = ({ navigation }) => {
-  const [selectedId, setSelectedId] = useState('0');
+
+  const [selectedDifficulty, setSelectedDifficulty] = useState('0');
+  const [isEndlessMode, setIsEndlessMode] = useState(false);
   const [selectedArif, setSelectedArif] = useState('0');
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => {
-    setIsEnabled(previousState => !previousState);
-  };
+  const toggleSwitch = () => setIsEndlessMode(prevState => !prevState);
 
   const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? '#223764' : '#86bfe8';
-    const color = item.id === selectedId ? 'white' : 'black';
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
+    const backgroundColor = item.id === selectedDifficulty ? '#223764' : '#86bfe8';
+    const color = item.id === selectedDifficulty ? 'white' : 'black';
+    return <Item item={item} onPress={() => setSelectedDifficulty(item.id)} backgroundColor={backgroundColor} textColor={color} />;
   };
+
   const renderItem2 = ({ item }) => {
     const backgroundColor = item.id === selectedArif ? '#223764' : '#86bfe8';
     const color = item.id === selectedArif ? 'white' : 'black';
-    return (
-      <Item2
-        item={item}
-        onPress={() => setSelectedArif(item.id)}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
+    return <Item item={item} onPress={() => setSelectedArif(item.id)} backgroundColor={backgroundColor} textColor={color} />;
   };
+
   return (
     <View style={styles.main}>
       <View style={styles.element1}>
-        <FlatList
-          horizontal
-          data={DATA_difficult}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          extraData={selectedId}
-        />
+        <FlatList horizontal data={DATA_difficult} renderItem={renderItem} keyExtractor={item => item.id} extraData={selectedDifficulty} contentContainerStyle={styles.centeredList} />
       </View>
       <View style={styles.element2}>
         <View>
-          <Image
-            style={styles.image}
-            source={require('../../src/graph.png')}
-          />
+          <Image style={styles.image} source={require('../../src/graph.png')} />
           <Text style={styles.image_text}>Ranked Game</Text>
           <Text style={styles.image_text}>(1 min)</Text>
         </View>
-        <Switch style={{ transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
-          trackColor={{ 1: '#223764', 0: '#223764' }}
-          thumbColor={isEnabled ? '#223764' : '#223764'}
-          ios_backgroundColor={'#223764'}
-          onValueChange={toggleSwitch}
-          value={isEnabled} />
+        <Switch style={styles.switch} trackColor={{ true: '#223764', false: '#223764' }} thumbColor={'#223764'} ios_backgroundColor={'#223764'} onValueChange={toggleSwitch} value={isEndlessMode} />
         <View>
-          <Image
-            style={styles.image}
-            source={require('../../src/infinite.png')}
-          />
+          <Image style={styles.image} source={require('../../src/infinite.png')} />
           <Text style={styles.image_text}>Endless Mode</Text>
         </View>
       </View>
       <View style={styles.element3}>
-        <FlatList
-          data={DATA_type}
-          renderItem={renderItem2}
-          keyExtractor={item => item.id}
-          extraData={selectedArif}
-          numColumns={3}
-        />
+        <FlatList data={DATA_type} renderItem={renderItem2} keyExtractor={item => item.id} extraData={selectedArif} contentContainerStyle={styles.centeredList} numColumns={3} />
       </View>
-      <TouchableOpacity style={styles.start}
-        onPress={() => {
-          if (isEnabled) {
-            navigation.navigate('EndlessGame', { dificult: selectedId, mode: isEnabled, type: selectedArif });
-          } else {
-            navigation.navigate('RankedGame', { dificult: selectedId, mode: isEnabled, type: selectedArif });
-          }
-        }
-        }>
+      <TouchableOpacity style={styles.start} onPress={() => navigation.navigate(isEndlessMode ? 'EndlessGame' : 'RankedGame', { dificult: selectedDifficulty, mode: isEndlessMode, type: selectedArif })}>
         <Text style={styles.text}>START GAME</Text>
       </TouchableOpacity>
-    </View>)
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: 'space-around',
-    padding: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
+    padding: width * 0.05,
     backgroundColor: '#ccc'
   },
   element1: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    //margin: 10,
-    paddingLeft: 8,
-    paddingTop: 8,
-    paddingBottom: 8,
+    padding: width * 0.02,
     borderRadius: 10,
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  centeredList: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1
   },
   element2: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    //margin: 10,
-    padding: 10,
+    padding: width * 0.02,
     borderRadius: 10,
-    alignItems: 'baseline',
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    backgroundColor: '#fff'
   },
   item: {
-    width: 90,
-    height: 90,
-    margin: 2,
+    width: width * 0.25,
+    height: width * 0.25,
+    margin: width * 0.005,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 5,
+    borderRadius: 5
   },
   image: {
-    margin: 15,
-    width: 90,
-    height: 80,
+    margin: width * 0.03,
+    width: width * 0.22,
+    height: width * 0.2
   },
   image_text: {
-    width: 110,
-    fontSize: 17,
-    textAlign: 'center',
+    width: width * 0.3,
+    fontSize: width * 0.045,
+    textAlign: 'center'
   },
   element3: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    //margin: 10,
-    paddingLeft: 8,
-    paddingTop: 8,
-    paddingBottom: 8,
+    padding: width * 0.02,
     borderRadius: 10,
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   start: {
     justifyContent: 'center',
-    //margin: 10,
-    padding: 30,
+    padding: height * 0.05,
     borderRadius: 10,
     alignItems: 'center',
-    backgroundColor: '#223764',
+    backgroundColor: '#223764'
   },
-  text:{
-    fontSize: 18,
+  text: {
+    fontSize: width * 0.05,
     fontWeight: 'bold',
-    color:'white',
+    color: 'white'
   },
-  title:{
-    fontSize: 18,
+  title: {
+    fontSize: width * 0.05
+  },
+  switch: {
+    transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }]
   }
 });
 
-export default Precise
+export default Precise;
