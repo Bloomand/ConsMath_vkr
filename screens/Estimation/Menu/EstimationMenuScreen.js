84cc32e080bf59003b0c7e5b0662331f0e0020d8
Bloomand <103037098+Bloomand@users.noreply.github.com>
2025-02-07 02:06:57 +0300

    refactor(styles): move EstimationMode screens styles to separate files

--- screens/Estimation/Menu/EstimationMenuScreen.js
+++ screens/Estimation/Menu/EstimationMenuScreen.js
@@ -10,7 +10,7 @@ import {
   Dimensions
 } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+import { styles } from './EstimationMenuScreen.styles';
 
 const DATA_difficult = [
   { id: '1', title: 'Medium' },
@@ -62,14 +62,25 @@ const EstimationMenuScreen = ({ navigation }) => {
           <Text style={styles.image_text}>Ranked Game</Text>
           <Text style={styles.image_text}>(1 min)</Text>
         </View>
-        <Switch style={styles.switch} trackColor={{ true: '#223764', false: '#223764' }} thumbColor={'#223764'} ios_backgroundColor={'#223764'} onValueChange={toggleSwitch} value={isEndlessMode} />
+        <Switch style={styles.switch}
+          trackColor={{ true: '#223764', false: '#223764' }}
+          thumbColor={'#223764'}
+          ios_backgroundColor={'#223764'}
+          onValueChange={toggleSwitch}
+          value={isEndlessMode} />
         <View>
           <Image style={styles.image} source={require('../../../assets/images/infinite.png')} />
           <Text style={styles.image_text}>Endless Mode</Text>
         </View>
       </View>
       <View style={styles.element3}>
-        <FlatList data={DATA_type} renderItem={renderItem2} keyExtractor={item => item.id} extraData={selectedArif} contentContainerStyle={styles.centeredList} numColumns={3} />
+        <FlatList
+          data={DATA_type}
+          renderItem={renderItem2}
+          keyExtractor={item => item.id}
+          extraData={selectedArif}
+          contentContainerStyle={styles.centeredList}
+          numColumns={3} />
       </View>
       <TouchableOpacity style={styles.start} onPress={() => navigation.navigate(isEndlessMode ? 'EndlessGameEstimation' : 'RankedGameEstimation', { dificult: selectedDifficulty, mode: isEndlessMode, type: selectedArif })}>
         <Text style={styles.text}>START GAME</Text>
@@ -77,79 +88,5 @@ const EstimationMenuScreen = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent: 'space-around',
-    padding: width * 0.05,
-    backgroundColor: '#ccc'
-  },
-  element1: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: width * 0.02,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  centeredList: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexGrow: 1
-  },
-  element2: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: width * 0.02,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  item: {
-    width: width * 0.25,
-    height: width * 0.25,
-    margin: width * 0.005,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5
-  },
-  image: {
-    margin: width * 0.03,
-    width: width * 0.22,
-    height: width * 0.2
-  },
-  image_text: {
-    width: width * 0.3,
-    fontSize: width * 0.045,
-    textAlign: 'center'
-  },
-  element3: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: width * 0.02,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  start: {
-    justifyContent: 'center',
-    padding: height * 0.05,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: '#223764'
-  },
-  text: {
-    fontSize: width * 0.05,
-    fontWeight: 'bold',
-    color: 'white'
-  },
-  title: {
-    fontSize: width * 0.05
-  },
-  switch: {
-    transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }]
-  }
-});
 
 export default EstimationMenuScreen;
